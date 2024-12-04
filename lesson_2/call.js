50c072097706961cec10be09db449ca66f875204
Espereon <announcement32@post.com>
2024-12-05 02:53:40 +0300

    l2_1

--- lesson_2/call.js
+++ lesson_2/call.js
@@ -11,3 +11,27 @@ const notARobot = {
     counterOfStarts: 10,
 };
 Roomba.startCleaning.call(notARobot); // I am cleaning... I have been started: 11 times.
+
+// Объект Roomba - робот-пылесос. Код самого объекта смотри в первом листинге.
+const notARobot2 = {
+    counterOfStarts: 10,
+};
+// Пример использования метода apply, для вызова функции с передачей в качестве контекста объекта notARobot и передачей в неё аргументов arg1, arg2, arg3.
+Roomba.startCleaning.apply(notARobot2, [arg1, arg2, arg3]); // I am cleaning... I have been started: 11 times.
+
+// Объект Roomba - робот-пылесос. Код самого объекта смотри в листинге 1.
+// Обращение к свойствам объекта.
+console.log(Roomba.model); // "Romba-1"
+console.log(Roomba.isFull); // false
+// Вызов методов объекта.
+// В setTimeout мы передаем не просто наш метод, а функцию, которая привязана к нашему объекту. Метод bind возвращает новую функцию, с уже привязанным контекстом, именно она вызывается по истечении времени.
+setTimeout(Roomba.startCleaning.bind(Roomba), 1000);
+// Установим свойства объекта isUVLampOn в true, чтобы продемонстрировать результат работы метода switchUVLamp.
+Roomba.isUVLampOn = true;
+// Результат вызова следующего метода зависит от значения, хранящегося в свойстве объекта, а также от того как этот метод был вызван (об этом поговорим чуть ниже).
+setTimeout(Roomba.switchUVLamp.bind(Roomba), 2000);
+setTimeout(Roomba.goCharge.bind(Roomba), 3000);
+// I am cleaning... I have been started: 1 times.
+// UV lamp is not working.
+// I am going to charge...
+
